1aaec6525096b6cd56f1e1d7b3b31f6cfe97a43b
Andre Christoga <andrechristoga@gmail.com>
2016-04-02 22:49:36 +0700

    New date variable, success alert

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 function submitForm() {
   var ref = new Firebase("https://dr-iqbal.firebaseio.com/"),
-      today = ref.child("today");
+      date = new Date(),
+  right_now = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate(),
+      today = ref.child(right_now);
 
   var nama = $('#nama').val(),
-      no_bpjs = $('#no_bpjs').val(),      
+      no_bpjs = $('#no_bpjs').val();
 
   today.push().set({
     nama: nama,
     no_bpjs: no_bpjs,
     date: date
   });
+
+  swal({
+    title: "Masih Tersedia",
+    text: "Anda masih bisa mendapatkan nomor antrian karena " + "<br>" + "masih ada tempat" + "<br>" + "<br>" + "Nomor antrian: 1",
+    type: "success",
+    html: true
+  })
 }
