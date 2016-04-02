59295cc71253ac15dfc486123b6761a185f825bc
Andre Christoga <andrechristoga@gmail.com>
2016-04-02 22:02:13 +0700

    Firebase depedencies

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+"use strict";
+var ref = new Firebase('https://dr-iqbal.firebaseio.com/');
+var today = ref.child("today");
+
 function submitForm() {
   var nama = $('#nama').val(),
       no_bpjs = $('#no_bpjs').val(),
