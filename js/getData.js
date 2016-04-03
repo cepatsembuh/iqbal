9b1485ebd4c58874c681f9aadce89e51e181dcc4
Andre Christoga <andrechristoga@gmail.com>
2016-04-04 00:59:24 +0700

    Shitty code that isn't working

--- js/getData.js
+++ js/getData.js
@@ -1,8 +1,17 @@
 window.onload = initializeData()
+var ref = new Firebase("https://cepatsembuh.firebaseio.com");
+var no_antri = new Firebase(ref + '/no_antrian');
 
 function initializeData() {
   var right_now = document.getElementById("right_now");
   var date = new Date();
   right_now.textContent = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
-  var right_now_text_content = right_now.textContent;  
+  var right_now_text_content = right_now.textContent;
 }
+
+ref.on("value", function(snapshot) {
+  var no_antri = document.getElementById("no_antri");
+  var no_antrian = snapshot.val();
+  no_antri.textContent = no_antrian;
+  var no_antrian_text_content = no_antri.textContent;
+})
