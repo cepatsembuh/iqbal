b5c30da12e80f77502f95fe194164a7099d4fd21
Andre Christoga <andrechristoga@gmail.com>
2016-04-04 00:56:01 +0700

    Long enough variable name

--- js/getData.js
+++ js/getData.js
@@ -4,5 +4,5 @@ function initializeData() {
   var right_now = document.getElementById("right_now");
   var date = new Date();
   right_now.textContent = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
-  var text = right_now.textContent;  
+  var right_now_text_content = right_now.textContent;  
 }
