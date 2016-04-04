60ad296bbdba9a14919a8b292f7089d35ba539f6
Andre Christoga <andrechristoga@gmail.com>
2016-04-04 17:12:40 +0700

    Moved no_antrian to initializeData() function

--- js/getData.js
+++ js/getData.js
@@ -7,11 +7,11 @@ function initializeData() {
   var date = new Date();
   right_now.textContent = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
   var right_now_text_content = right_now.textContent;
-}
 
-ref.on("value", function(snapshot) {
-  var no_antri = $('#no_antri');
-  var no_antrian = snapshot.val();
-  no_antri.textContent = no_antrian;
-  var no_antrian_text_content = no_antri.textContent;
-})
+  ref.on("value", function(snapshot) {
+    var no_antri = $('#no_antri');
+    var no_antrian = snapshot.val();
+    no_antri.textContent = no_antrian;
+    var no_antrian_text_content = no_antri.textContent;
+  })
+}
