63dbcbe2fa8a1ca0fb1dbc1e4df928f11e52f45e
Andre Christoga <andrechristoga@gmail.com>
2016-04-04 17:10:16 +0700

    Use jQuery to get elements

--- js/getData.js
+++ js/getData.js
@@ -3,14 +3,14 @@ var ref = new Firebase("https://cepatsembuh.firebaseio.com");
 var no_antri = new Firebase(ref + '/no_antrian');
 
 function initializeData() {
-  var right_now = document.getElementById("right_now");
+  var right_now = $('#right_now');
   var date = new Date();
   right_now.textContent = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
   var right_now_text_content = right_now.textContent;
 }
 
 ref.on("value", function(snapshot) {
-  var no_antri = document.getElementById("no_antri");
+  var no_antri = $('#no_antri');
   var no_antrian = snapshot.val();
   no_antri.textContent = no_antrian;
   var no_antrian_text_content = no_antri.textContent;
