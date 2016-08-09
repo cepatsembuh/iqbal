39b38243a4afb87bde82a9400ab7d9d0a14f2a4f
Andre Christoga <andrechristoga@gmail.com>
2016-08-09 14:49:31 +0700

    Clean code.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function submitForm() {
   // Firebase ref
-  var ref = new Firebase("https://dr-iqbal.firebaseio.com/");
+  var ref = new Firebase("https://dr-iqbal.firebaseio.com/"),
+      nomor = new Firebase("https://dr-iqbal.firebaseio.com/no_antrian");
   
   // Today date's
   var date = new Date(),
@@ -10,14 +11,6 @@ function submitForm() {
       right_now = year + '-' + month + '-' + day,
       today = ref.child(right_now);
 
-  // Tommorow date's
-  var tommorow = date.getDate() + 1,
-      tommorow_date = year + '-' + month + '-' + tommorow,
-      tommorow_child = ref.child(tommorow_date);
-
-  // Number ref
-  var nomor = new Firebase("https://dr-iqbal.firebaseio.com/no_antrian");
-
   // Get input value
   var input = $('.input').val(),
       nama = $('#nama').val(),
@@ -26,62 +19,61 @@ function submitForm() {
       no_rujuk = $('#no_rujuk').val(),
       keluhan = $('#keluhan').val();
 
+  function pesan() {
+    nomor.transaction(function(currentRank){
+      update = currentRank + 1;
 
-      function pesan() {
-        nomor.transaction(function(currentRank){
-          update = currentRank + 1;
-
-          return update;
-        }, function(error, commited, snapshot){
-          if (error) {
-            swal({
-              title: "Gagal mengambil data",
-              text: "Gagal mengambil data, coba cek kembali koneksi anda",
-              type: "error"
-            })
-          } else {
-              swal({
-                title: "Terdaftar",
-                text: "Anda terdaftar pada: " + right_now,
-                type: "success",
-              }, function() {                                    
-                location.reload();
-              })
-              
-              today.push().set({
-                nama: nama,
-                no_bpjs: no_bpjs,
-                nik: nik,
-                no_rujuk: no_rujuk,
-                keluhan: keluhan,
-                date: date,
-                no_antri: snapshot.val()
-              })
-          }
-        })
-      }
-
-      // Validate user input
-      if (input === '' || keluhan.length < 10 || nik.length != 16 || no_bpjs.length != 13 || no_rujuk.length != 20) {        
+      return update;
+    }, function(error, commited, snapshot){
+      if (error) {
         swal({
-          title: "Input tidak valid",
-          text: "Salah satu input anda tidak valid",
+          title: "Gagal mengambil data",
+          text: "Gagal mengambil data, coba cek kembali koneksi anda",
           type: "error"
         })
       } else {
-          nomor.on('value', function(snapshot){
-            if (snapshot.val() >= 40) {
-              swal({
-                title: "Kuota pasien sudah penuh",
-                text: "Kuota pasien untuk hari ini sudah penuh" + "<br>" + "Cobalah mendaftar besok",
-                type: "warning",
-                html: true
-              }, function() {                    
-                location.reload();
-              })
-            } else {
-                pesan();
-            }
-          })          
-      } // ./else
-    }
+          swal({
+            title: "Terdaftar",
+            text: "Anda terdaftar pada: " + right_now,
+            type: "success",
+          }, function() {                                    
+            location.reload();
+          })
+          
+          today.push().set({
+            nama: nama,
+            no_bpjs: no_bpjs,
+            nik: nik,
+            no_rujuk: no_rujuk,
+            keluhan: keluhan,
+            date: date,
+            no_antri: snapshot.val()
+          })
+        }
+    })
+  }
+
+  // Validate user input
+  if (input === '' || keluhan.length < 10 || nik.length != 16 || no_bpjs.length != 13 || no_rujuk.length != 20) {        
+    swal({
+      title: "Input tidak valid",
+      text: "Salah satu input anda tidak valid",
+      type: "error"
+    })
+  } else {
+      nomor.on('value', function(snapshot){
+        if (snapshot.val() >= 40) {
+          swal({
+            title: "Kuota pasien sudah penuh",
+            text: "Kuota pasien untuk hari ini sudah penuh" + "<br>" + "Cobalah mendaftar besok",
+            type: "warning",
+            html: true
+          }, function() {                    
+            location.reload();
+          })
+        } else {
+            pesan();
+        }
+      })           
+    } // ./else
+  }
